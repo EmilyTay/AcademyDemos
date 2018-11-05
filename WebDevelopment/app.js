c99167a6cb6ad482efad152a9682f51b0dd53aad
Emily Taylor <Emily.Taylor@Softwire.com>
2018-11-05 15:27:15 +0000

    Web development - setting up a simple express server with some endpoints and static content

---
+++ WebDevelopment/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// These 2 endpoints return a simple text string
+// app.get('/hello', (req, res) => res.send('Hello World!'))
+// app.get('/bye', (req, res) => res.send('Bye World!'))
+
+// This endpoint exposes all the static resources in the frontend directory
+app.use('/frontend', express.static('frontend'));
+
+// This endpoint performs some logic and returns a variable to the client
+app.get('/dosomething', function (req, res) {
+  var x = 5;
+  res.send(x);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
